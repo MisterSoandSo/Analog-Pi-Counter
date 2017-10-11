3fce6f45932506c073707421cd0a5472985d443a
Andrew So <aso001@ucr.edu>
2017-10-11 10:40:20 -0700

    Update two_int.s

--- Assignments/Two Integers/two_int.s
+++ Assignments/Two Integers/two_int.s
@@ -1,26 +1,41 @@
-.section .data
+//Andrew So
+
+.global main
+
+.section .rodata
 
 prompt:     .asciz "Hello! Welcome to Two Integers.\n\n"
 prompt_1:   .asciz "Enter an integer for first number:"
 prompt_2:   .asciz "Enter an integer for second number:"
+int_in:     .asciz "%d"
 
+.section .data
 number_1:   .word 0 
 number_2:   .word 0 
 sum:        .word 0 
 difference: .word 0 
 product:    .word 0 
 
-.global main
-
-main: 	push {lr}         /*save our return address*/
-        ldr r0, =prompt   /**/
-        bl printf         /*Call printf to output*/
+main: 	push {lr}         	/*save our return address*/
+        
+	ldr r0, =prompt  	/*Load welcome msg*/
+        bl printf         	/*Call printf to output*/
 	
-	ldr r0, =prompt_1   /**/
-        bl printf         /*Call printf to output*/
+	ldr r0, =prompt_1   	/*load 1st msg*/
+        bl printf        	/*Call printf to output*/
 	
-	ldr r0, =prompt_2   /**/
-        bl printf         /*Call printf to output*/
+	ldr r0, =int_in		/*load input format*/
+        ldr r1, =number_1	/*read user input*/
+        bl scanf
+	
+	ldr r0, =prompt_2   	/*load 2nd msg*/
+        bl printf        	/*Call printf to output*/
+	
+	ldr r0, =int_in		/*load input format*/
+        ldr r1, =number_2	/*read user input*/
+        bl scanf
+	
+	/*Finished taking in user input*/
 
   // assembly program here
 
